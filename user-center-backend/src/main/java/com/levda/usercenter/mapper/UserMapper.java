7740ed48b31962f9d62f41b75acc23a1f0ce2f6b
YuhanW <wangyuhan6459@163.com>
2026-01-03 14:38:32 +0000

    feat: add user entity and database mapper layer

---
+++ user-center-backend/src/main/java/com/levda/usercenter/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.levda.usercenter.mapper;/*
+ * @author BlairWang
+ * @Date 30/12/2025 3:23 pm
+ * @Version 1.0
+ */
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.levda.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ user-center-backend/src/main/java/com/levda/usercenter/model/User.java
@@ -0,0 +1,34 @@
+package com.levda.usercenter.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@TableName(value="user")
+public class User implements Serializable {
+
+    private Long id;
+    private String username; // 昵称
+    private String userAccount; //账户号
+    private String avatarUrl;
+    private Integer gender;
+    private String userPassword;
+    private String phone;
+    private String email;
+    private Integer userStatus;
+    private Date createTime;
+    private Date updateTime;
+    @TableLogic
+    private Integer isDelete;
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+}
